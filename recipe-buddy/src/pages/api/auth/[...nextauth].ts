c08eda8fe6f0cdbe5eadfc7a840b5c2945dbacd7
George Gebbett <george.gebbett1@gmail.com>
2023-04-30 11:37:19 +0300

    Add t3-app scaffold

---
+++ recipe-buddy/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,4 @@
+import NextAuth from "next-auth";
+import { authOptions } from "@recipe-buddy/server/auth";
+
+export default NextAuth(authOptions);
---
+++ recipe-buddy/src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,19 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { env } from "@recipe-buddy/env.mjs";
+import { createTRPCContext } from "@recipe-buddy/server/api/trpc";
+import { appRouter } from "@recipe-buddy/server/api/root";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+          );
+        }
+      : undefined,
+});
